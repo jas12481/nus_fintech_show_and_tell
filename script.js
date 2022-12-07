6bf01eaeea4a564fdb5c8d3b4ca24423babc1603
jas12481 <116910080+jas12481@users.noreply.github.com>
2022-12-08 01:42:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,24 @@
+var heightInput = document.querySelector(".height-input-field");
+var weightInput = document.querySelector(".weight-input-field");
+var calculateButton = document.querySelector(".calculate");
+var result = document.querySelector(".result");
+var statement = document.querySelector(".result-statement");
+var BMI, height, weight;
 
+calculateButton.addEventListener("click", ()=>{
 
-function findbmi() {
+    height = heightInput.value;
+    weight = weightInput.value;
+    BMI = weight/(height**2); 
+    result.innerText = BMI;
 
-	var w = document.getElementById("weight").value;
-	var h1 = document.getElementById("heightft").value;
-	var h2 = document.getElementById("heightin").value;
-
-	if (w === '') {
-		alert("Enter weight");
-	} else if (h1 === '') {
-		alert("Enter height in ft");
-	} else if (h2 === '') {
-		alert("Enter height in inches");
-	} else {
-
-		var temp = (parseInt(h1) * 12 + parseInt(h2));
-		var res = 703 * parseInt(w)/(temp * temp); // 703*(weight/height^2)
-		res = Math.round(100*res)/100; // rounds to nearest hundredth
-
-
-		document.getElementById("result").innerText = "Your BMI is: " + res;
-		document.getElementById("result").style.display = "block";
-
-		var suggest = document.getElementById("suggest");
-		if (res < 18.5) {
-			suggest.innerText = "You are underweight :(";
-
-			suggest.style.display = "block";
-			
-
-		} else if (res > 18.5 && res < 24.9) {
-			suggest.innerHTML = "You are normal weight :)";
-			suggest.style.display = "block";
-		} else if (res > 25 && res < 29.9) {
-			suggest.innerHTML = "You are over weight :(";
-			suggest.style.display = "block";
-		} else if (res > 30) {
-			suggest.innerHTML = "You are obese :(";
-			suggest.style.display = "block";
-		} else {
-			suggest.innerHTML = "Did you enter correct measurements?";
-			suggest.style.display = "block";
-		}
-
-		//alert("Your BMI is: " + res);
-
-		document.getElementById("weight").value = "";
-		document.getElementById("heightft").value = "";
-		document.getElementById("heightin").value = "";
-	} 
-}
+    if(BMI < 18.5){
+        statement.innerText = "Your BMI falls within the underweight range";    
+    }else if((BMI > 18.5) && (BMI < 24.9)){
+        statement.innerText = "Your BMI falls within the normal or healthy weight range";
+    }else if((BMI > 25) && (BMI < 29.9 )){
+        statement.innerText = "Your BMI falls within the overweight range";
+    }else{
+        statement.innerText = "Your BMI falls within the obese range";
+    }
+});
